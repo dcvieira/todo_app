083071d955de5ab4111d1db669bc002fffd51d6c
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2022-07-31 19:22:12 -0300

    refatoração

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todo_app/models/app_state.dart';
-import 'package:todo_app/models/todo.dart';
 import 'package:todo_app/pages/add_edit_page.dart';
 import 'package:todo_app/pages/home_page.dart';
 import 'package:todo_app/provider/todo_list_provider.dart';
@@ -24,25 +22,6 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
-  AppState appState = AppState.loading();
-
-  @override
-  void initState() {
-    super.initState();
-
-    widget.repository.loadTodos().then((loadedTodos) {
-      setState(() {
-        appState = AppState(
-          todos: loadedTodos.toList(),
-        );
-      });
-    }).catchError((err) {
-      setState(() {
-        appState.isLoading = false;
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -57,54 +36,6 @@ class _TodoAppState extends State<TodoApp> {
           '/addTodo': (context) => const AddEditPage()
         },
       ),
-    );
-  }
-
-  void toggleAll() {
-    setState(() {
-      appState.toggleAll();
-    });
-  }
-
-  void clearCompleted() {
-    setState(() {
-      appState.clearCompleted();
-    });
-  }
-
-  void addTodo(Todo todo) {
-    setState(() {
-      appState.todos.add(todo);
-    });
-  }
-
-  void removeTodo(Todo todo) {
-    setState(() {
-      appState.todos.remove(todo);
-    });
-  }
-
-  void updateTodo(
-    Todo todo, {
-    bool? complete,
-    String? id,
-    String? note,
-    String? task,
-  }) {
-    setState(() {
-      todo.complete = complete ?? todo.complete;
-      todo.id = id ?? todo.id;
-      todo.note = note ?? todo.note;
-      todo.task = task ?? todo.task;
-    });
-  }
-
-  @override
-  void setState(VoidCallback fn) {
-    super.setState(fn);
-
-    widget.repository.saveTodos(
-      appState.todos.toList(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_app/models/app_state.dart';
 import 'package:todo_app/models/enums.dart';
-import 'package:todo_app/models/typedefs.dart';
-import 'package:todo_app/provider/todo_list_provider.dart';
 import 'package:todo_app/util/app_keys.dart';
 import 'package:todo_app/widgets/extra_actions_button.dart';
 import 'package:todo_app/widgets/filter_button.dart';
@@ -40,7 +36,7 @@ class HomePageState extends State<HomePage> {
           const ExtraActionsButton()
         ],
       ),
-      body: activeTab == AppTab.todos ? TodoList() : StatsCounter(),
+      body: activeTab == AppTab.todos ? const TodoList() : const StatsCounter(),
       floatingActionButton: FloatingActionButton(
         key: AppKeys.addTodoFab,
         onPressed: () {
